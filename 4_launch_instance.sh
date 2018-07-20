474a9b7ce531ba516f84e39d13b02e37785696b9
NileshChandekar <deejniels@gmail.com>
2018-07-20 15:15:56 +0530

    README

--- 4_launch_instance.sh
+++ 4_launch_instance.sh
@@ -48,7 +48,7 @@ date=$(date +"%Y-%m-%d_%H-%M-%S")
 
 echo "##### LAUNCH AN CIRROS INSTANCE #####"
 echo "====================================="
-nova boot --flavor $FLAVOR --image $GLANCE --nic net-id=$NET  --security-groups default cirros_$date ; sleep 15 
+nova boot --flavor $FLAVOR --image $GLANCE --nic net-id=$NET  --security-groups default cirros_$(date) ; sleep 15 
 clear
 echo "==================================="
 echo "##### CIRROS INSTANCE CREATED #####"
